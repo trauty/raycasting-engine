9bbcf0581cbd22b9b0d7d1f3fbd63260f5e1a882
Ratchgamer123 <trautwein.maximilian04@gmail.com>
2022-10-16 16:56:34 +0200

    full dynamic raycasting with minimap done

--- src/main.c
+++ src/main.c
@@ -10,32 +10,32 @@
 #include "shaderCode.h"
 #include "bufferUtil.h"
 
-#define PI 3.14159265359
-#define DR 0.0174533
-
-#define MAP_WIDTH 8
-#define MAP_HEIGHT 8
-#define MAP_SIZE 64
-#define MAP_GRID_SIZE 32
+#define MAP_WIDTH 16
+#define MAP_HEIGHT 16
+#define MAP_SIZE 128
 
 GLFWwindow* window = NULL;
 
 int screenWidth = 1280, screenHeight = 720;
+int minimapOffset = 0;
 
-float playerPosX = 200.0f, playerPosY = 200.0f;
-float playerDeltaX = 0.0f, playerDeltaY = 0.0f;
-float playerAngle = 0.0f;
-float rotationSpeed = 4.0f;
+float playerPosX = 2.0, playerPosY = 3.0;
+float dirX = -1.0, dirY = 0.0;
+float planeX = 0.0, planeY = 0.66;
+float rotationSpeed = 4.0;
 
-int maxRays = 1;
+float playerDeltaX = 0.0f;
+float playerDeltaY = 0.0f;
 
-float movementSpeed = 250.0f;
+float movementSpeed = 4.0f;
 
 Vertex playerVertex;
 
+Vertex playerDirVertices[2];
+
 Vertex minimapVertices[2048];
 
-Vertex raycastVertices[2048];
+Vertex raycastVertices[4096];
 
 Vertex worldVertices[2048];
 
@@ -51,16 +51,24 @@ float deltaTime = 0.0f;
 float currentFrame = 0.0f;
 float previousFrame = 0.0f;
 
-int map[]= 
+int map[MAP_WIDTH][MAP_HEIGHT] =
 {
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 0, 1, 0, 0, 0, 0, 1,
-    1, 0, 1, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 1, 1, 1, 0, 1,
-    1, 0, 0, 1, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 1,
-    1, 1, 1, 1, 1, 1, 1, 1
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1},
+    {1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 };
 
 void calculateDeltaTime()
@@ -90,37 +98,40 @@ void windowSizeCallback(GLFWwindow* window, int width, int height)
 
 void input()
 {
+    float deltaRot = rotationSpeed * deltaTime;
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
     {
-        playerAngle += rotationSpeed * deltaTime;
+        float oldDirX = dirX;
+        dirX = dirX * cos(-deltaRot) - dirY * sin(-deltaRot);
+        dirY = oldDirX * sin(-deltaRot) + dirY * cos(-deltaRot);
         
-        if (playerAngle > 2 * PI)
-        {
-            playerAngle -= 2 * PI;
-        }
+        float oldPlaneX = planeX;
+        planeX = planeX * cos(-deltaRot) - planeY * sin(-deltaRot);
+        planeY = oldPlaneX * sin(-deltaRot) + planeY * cos(-deltaRot);
     }
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
     {
-        playerAngle -= rotationSpeed * deltaTime;
+        float oldDirX = dirX;
+        dirX = dirX * cos(deltaRot) - dirY * sin(deltaRot);
+        dirY = oldDirX * sin(deltaRot) + dirY * cos(deltaRot);
         
-        if (playerAngle < 0)
-        {
-            playerAngle += 2 * PI;
-        }
+        float oldPlaneX = planeX;
+        planeX = planeX * cos(deltaRot) - planeY * sin(deltaRot);
+        planeY = oldPlaneX * sin(deltaRot) + planeY * cos(deltaRot);
     }
 
-    playerDeltaX = cos(playerAngle) * movementSpeed;
-    playerDeltaY = sin(playerAngle) * movementSpeed;
+    playerDeltaX = dirX * movementSpeed * deltaTime;
+    playerDeltaY = dirY * movementSpeed * deltaTime;
     
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
     {
-        playerPosX += playerDeltaX * deltaTime;
-        playerPosY += playerDeltaY * deltaTime;
+        if (map[(int)(playerPosX + dirX)][(int)playerPosY] == 0) { playerPosX += playerDeltaX; }
+        if (map[(int)playerPosX][(int)(playerPosY + dirY)] == 0) { playerPosY += playerDeltaY; }
     }
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
     {
-        playerPosX -= playerDeltaX * deltaTime;
-        playerPosY -= playerDeltaY * deltaTime;
+        if (map[(int)(playerPosX - dirX)][(int)playerPosY] == 0) { playerPosX -= playerDeltaX; }
+        if (map[(int)playerPosX][(int)(playerPosY - dirY)] == 0) { playerPosY -= playerDeltaY; }
     }
 }
 
@@ -129,187 +140,144 @@ void initMinimap()
     int xOffset = 0, yOffset = 0, minimapArrOffset = 0;
     for (int x = 0; x < MAP_WIDTH; x++)
     {
-        for (int y = 0; y < MAP_WIDTH; y++)
+        for (int y = 0; y < MAP_HEIGHT; y++)
         {
             GLfloat color = 0.0f;
-            if (map[y * MAP_WIDTH + x] == 1)
+            if (map[x][y] == 1)
             {
                 color = 1.0f;
             }
 
-            xOffset = x * MAP_SIZE / 4;
-            yOffset = y * MAP_SIZE / 4;
+            minimapOffset = screenHeight - MAP_SIZE;
 
-            newVertex(&minimapVertices[minimapArrOffset] ,xOffset, yOffset, color, color, color);    
-            newVertex(&minimapVertices[minimapArrOffset + 1], xOffset, yOffset + MAP_SIZE / 4, color, color, color);
-            newVertex(&minimapVertices[minimapArrOffset + 2], xOffset + MAP_SIZE / 4, yOffset + MAP_SIZE / 4, color, color, color);
+            xOffset = x * MAP_SIZE / MAP_WIDTH;
+            yOffset = y * MAP_SIZE / MAP_HEIGHT + minimapOffset;
+
+            newVertex(&minimapVertices[minimapArrOffset], xOffset, yOffset, color, color, color);    
+            newVertex(&minimapVertices[minimapArrOffset + 1], xOffset, yOffset + MAP_SIZE / MAP_HEIGHT, color, color, color);
+            newVertex(&minimapVertices[minimapArrOffset + 2], xOffset + MAP_SIZE / MAP_WIDTH, yOffset + MAP_SIZE / MAP_HEIGHT, color, color, color);
             newVertex(&minimapVertices[minimapArrOffset + 3], xOffset, yOffset, color, color, color);
-            newVertex(&minimapVertices[minimapArrOffset + 4], xOffset + MAP_SIZE / 4, yOffset, color, color, color);
-            newVertex(&minimapVertices[minimapArrOffset + 5], xOffset + MAP_SIZE / 4, yOffset + MAP_SIZE / 4, color, color, color);
+            newVertex(&minimapVertices[minimapArrOffset + 4], xOffset + MAP_SIZE / MAP_WIDTH, yOffset, color, color, color);
+            newVertex(&minimapVertices[minimapArrOffset + 5], xOffset + MAP_SIZE / MAP_WIDTH, yOffset + MAP_SIZE / MAP_HEIGHT, color, color, color);
         
             minimapArrOffset += 6;
         }
     }
 }
 
-void drawRays()
+void raycast()
 {
-    float rayAngle = 0.0f, xOffset = 0.0f, yOffset = 0.0f, distT = 0.0f;
-    float rayStartX = playerPosX, rayStartY = playerPosY;
-
     int lineVerticesOffset = 0;
     int worldVerticesOffset = 0;
 
-    rayAngle = playerAngle;
-    if (rayAngle < 0)
+    for (int rays = 0; rays < screenWidth; rays++)
     {
-        rayAngle += 2 * PI;
-    }
-    else if (rayAngle > 2 * PI)
-    {
-        rayAngle -= 2 * PI;
-    }
+        float cameraX = 2.0f * rays / (float)screenWidth - 1.0f;
+        float rayDirX = dirX + planeX * cameraX;
+        float rayDirY = dirY + planeY * cameraX;
 
+        int mapX = (int)playerPosX;
+        int mapY = (int)playerPosY;
 
-    for (int rays = 0; rays < maxRays; rays++)
-    {
-        float rayDirX = cos(rayAngle), rayDirY = sin(rayAngle);
-        float rayUnitStepSizeX = sqrt(1 + ((rayDirY / rayDirX) * (rayDirY / rayDirX)));
-        float rayUnitStepSizeY = sqrt(1 + ((rayDirX / rayDirY) * (rayDirX / rayDirY)));
-        int mapCheckX = (int)(rayStartX / MAP_GRID_SIZE), mapCheckY = (int)(rayStartY / MAP_GRID_SIZE);
-        float rayLength1DX = 0.0f, rayLength1DY = 0.0f;
-        float stepX = 0.0f, stepY = 0.0f;
+        float sideDistX = 0.0f;
+        float sideDistY = 0.0f;
+
+        float deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1.0 / rayDirX);
+        float deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1.0 / rayDirY);
+        float perpWallDist = 0.0f;
+
+        int stepX = 0;
+        int stepY = 0;
+
+        int hit = 0;
+        int side = 0;
 
         if (rayDirX < 0)
         {
             stepX = -1;
-            rayLength1DX = (rayStartX - (float)mapCheckX) * rayUnitStepSizeX;
+            sideDistX = (playerPosX - mapX) * deltaDistX;
         }
         else
         {
             stepX = 1;
-            rayLength1DX = (((float)mapCheckX + 1) - rayStartX) * rayUnitStepSizeX;
+            sideDistX = (mapX + 1.0f - playerPosX) * deltaDistX;
         }
         if (rayDirY < 0)
         {
             stepY = -1;
-            rayLength1DY = (rayStartY - (float)mapCheckY) * rayUnitStepSizeY;
+            sideDistY = (playerPosY - mapY) * deltaDistY;
         }
         else
         {
             stepY = 1;
-            rayLength1DY = (((float)mapCheckY + 1) - rayStartY) * rayUnitStepSizeY;
+            sideDistY = (mapY + 1.0f - playerPosY) * deltaDistY;
         }
 
-        bool tileFound = false;
-        float maxDistance = 1000.0f;
-        float distance = 0.0f;
-        while (!tileFound && distance < maxDistance)
+        while (hit == 0)
         {
-            if (rayLength1DX < rayLength1DY)
+            if (sideDistX < sideDistY)
             {
-                mapCheckX += stepX;
-                distance = rayLength1DX;
-                rayLength1DX += rayUnitStepSizeX;
+                sideDistX += deltaDistX;
+                mapX += stepX;
+                side = 0;
             }
             else
             {
-                mapCheckY += stepY;
-                distance = rayLength1DY;
-                rayLength1DY += rayUnitStepSizeY;
+                sideDistY += deltaDistY;
+                mapY += stepY;
+                side = 1;
             }
-
-            if (mapCheckX >= 0 && mapCheckX < MAP_WIDTH && mapCheckY >= 0 && mapCheckY < MAP_HEIGHT)
+            
+            if (map[mapX][mapY] > 0)
             {
-                if (map[mapCheckY * MAP_WIDTH + mapCheckX] == 1)
-                {
-                    tileFound = true;
-                }
+                hit = 1;
             }
         }
-
-        float intersectionX, intersectionY;
-        if (tileFound)
+        
+        if (side == 0)
         {
-            intersectionX = rayStartX + rayDirX * distance;
-            intersectionY = rayStartY + rayDirY * distance;
+            perpWallDist = (sideDistX - deltaDistX);
+        }
+        else
+        {
+            perpWallDist = (sideDistY - deltaDistY);
         }
 
-        //printf("%f : %f \n", rayUnitStepSizeX, rayUnitStepSizeY);
-        printf("%f : %f \n", rayDirX, rayDirY);
+        if (cameraX == 0)
+        {
+            newVertex(&playerDirVertices[0], playerVertex.x, playerVertex.y, 0.0f, 1.0f, 0.0f);
+            newVertex(&playerDirVertices[1], playerVertex.x + dirX * perpWallDist * MAP_SIZE / MAP_WIDTH, playerVertex.y + dirY * perpWallDist * MAP_SIZE / MAP_HEIGHT, 0.0f, 1.0f, 0.0f);
+        }
 
-        float testX = rayStartX + rayDirX * 100.0f;
-        float testY = rayStartY + rayDirY * 100.0f;
+        int lineHeight = (int)(screenHeight / perpWallDist);
 
-        // float color = 0.0f;
+        int drawStart = -lineHeight / 2 + screenHeight / 2;
+        if (drawStart < 0)
+        {
+            drawStart = 0;
+        }
         
-        // if (distVert < distHor)
-        // {
-        //     rayX = vertX;
-        //     rayY = vertY;
-        //     distT = distVert;
-        //     color = 0.9f;
-        // }
-        // else
-        // {
-        //     rayX = horX;
-        //     rayY = horY;
-        //     distT = distHor;
-        //     color = 0.7f;
-        // }
+        int drawEnd = lineHeight / 2 + screenHeight / 2;
+        if (drawEnd >= screenHeight)
+        {
+            drawEnd = screenHeight - 1;
+        }
 
-        newVertex(&raycastVertices[lineVerticesOffset], playerPosX, playerPosY, 0.1f, 1.0f, 0.1f);
-        newVertex(&raycastVertices[lineVerticesOffset + 1], intersectionX, intersectionY, 0.1f, 1.0f, 0.1f);
-        newVertex(&raycastVertices[lineVerticesOffset + 2], playerPosX, playerPosY, 0.1f, 1.0f, 0.1f);
-        newVertex(&raycastVertices[lineVerticesOffset + 3], testX, testY, 1.0f, 0.0f, 0.1f);
+        float greenColor = 0.0f;
+        if (side == 1)
+        {
+            greenColor = 0.6f;
+        }
+        else
+        {
+            greenColor = 1.0f;
+        }
 
-        lineVerticesOffset += 4;
+        newVertex(&raycastVertices[lineVerticesOffset], rays, drawStart, 0.1f, greenColor, 0.1f);
+        newVertex(&raycastVertices[lineVerticesOffset + 1], rays, drawEnd, 0.1f, greenColor, 0.1f);
 
-        // float cosAngle = playerAngle - rayAngle;
-        
-        // if (cosAngle < 0)
-        // {
-        //     cosAngle += 2 * PI;
-        // }
-        // else if (cosAngle > 2 * PI)
-        // {
-        //     cosAngle -= 2 * PI;
-        // }
-
-        // distT *= cos(cosAngle);
-
-        // float lineHeight = (MAP_SIZE * screenHeight) / distT;
-        // if (lineHeight > screenHeight)
-        // {
-        //     lineHeight = screenHeight;
-        // }
-
-        // float lineOffset = screenHeight / 2 - lineHeight / 2;
-
-        // float screenScale = screenWidth / maxRays + 1;
-
-        // newVertex(&worldVertices[worldVerticesOffset], rays * screenScale, lineOffset, 0.1f, color, 0.1f);
-        // newVertex(&worldVertices[worldVerticesOffset + 1], rays * screenScale, lineHeight + lineOffset, 0.1f, color, 0.1f);
-        // newVertex(&worldVertices[worldVerticesOffset + 2], rays * screenScale + screenScale, lineOffset, 0.1f, color, 0.1f);
-        // newVertex(&worldVertices[worldVerticesOffset + 3], rays * screenScale, lineHeight + lineOffset, 0.1f, color, 0.1f);
-        // newVertex(&worldVertices[worldVerticesOffset + 4], rays * screenScale + screenScale, lineHeight + lineOffset, 0.1f, color, 0.1f);
-        // newVertex(&worldVertices[worldVerticesOffset + 5], rays * screenScale + screenScale, lineOffset, 0.1f, color, 0.1f);
-
-        // worldVerticesOffset += 6;
-
-        rayAngle += DR / 2;
-
-        // if (rayAngle < 0)
-        // {
-        //     rayAngle += 2 * PI;
-        // }
-        // else if (rayAngle > 2 * PI)
-        // {
-        //     rayAngle -= 2 * PI;
-        // }
+        lineVerticesOffset += 2;
     }
-
-    
 }
 
 int main()
@@ -333,11 +301,11 @@ int main()
 
     glfwSetWindowSizeCallback(window, windowSizeCallback);
 
-    glfwSwapInterval(1);
+    glfwSwapInterval(0);
 
     gladLoadGL();
 
-   // initMinimap();
+    initMinimap();
 
     glViewport(0, 0, screenWidth, screenHeight);
 
@@ -349,7 +317,9 @@ int main()
 
     GLuint shaderID = createShader(vertexShaderSource, fragmentShaderSource);
 
-    ArrayBuffers* playerBuffers = genArrayBuffers(&playerVertex, sizeof(playerVertex), false);
+    ArrayBuffers* playerBuffers = genArrayBuffers(&playerVertex, sizeof(playerVertex), true);
+    
+    ArrayBuffers* playerDirBuffers = genArrayBuffers(playerDirVertices, sizeof(playerVertex), true);
 
     ArrayBuffers* minimapBuffers = genArrayBuffers(minimapVertices, sizeof(minimapVertices), false);
 
@@ -359,9 +329,10 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
-        drawRays();
-        playerVertex.x = playerPosX;
-        playerVertex.y = playerPosY;
+        raycast();
+        
+        playerVertex.x = playerPosX * MAP_SIZE / MAP_WIDTH;
+        playerVertex.y = playerPosY * MAP_SIZE / MAP_HEIGHT + minimapOffset;
 
         glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -386,6 +357,11 @@ int main()
 
         glBindVertexArray(minimapBuffers->vao);
         glDrawArrays(GL_TRIANGLES, 0, sizeof(minimapVertices) / sizeof(GLfloat));
+
+        glBindVertexArray(playerDirBuffers->vao);
+        glBindBuffer(GL_ARRAY_BUFFER, playerDirBuffers->vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(playerDirVertices), playerDirVertices, GL_DYNAMIC_DRAW);
+        glDrawArrays(GL_LINES, 0, sizeof(playerDirVertices) / sizeof(GLfloat));
 
         glBindVertexArray(playerBuffers->vao);
         glBindBuffer(GL_ARRAY_BUFFER, playerBuffers->vbo);
